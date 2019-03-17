4d3a30da633462bc992ce2986d5b7feb6b7785e1
Carla Gramichi <caaar.m21@gmail.com>
2019-03-17 15:55:54 -0300

    listado de comentarios

--- listado_comentarios.php
+++ listado_comentarios.php
@@ -3,8 +3,24 @@ session_start();
 require 'conexion.php';
 $con = conectar();
 
-$consulta = $con->query("SELECT comentario FROM comentarios ");
+$consulta = $con->query("SELECT nombre,comentario FROM comentarios ");
 
 $coment = $consulta->fetchAll(PDO::FETCH_ASSOC);
 require 'plantillas/cabecera.php';
 ?>
+
+<body class="container">
+<h1>Comentarios registrados:</h1>
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <th>Nombre</th>
+        <th>Comentario</th>
+    </tr>
+    </thead>
+    <?php foreach ($coment as $comenta): ?>
+    <tr>
+        <td><?= $comenta['nombre'] ?></td>
+        <td><?= $comenta['comentario'] ?></td>
+    </tr>
+    <?php endforeach;
