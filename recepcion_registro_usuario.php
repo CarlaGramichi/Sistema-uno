d6820546afa1d4d4ee411e62587e945a10ac3e33
Carla Gramichi <caaar.m21@gmail.com>
2019-03-22 13:29:58 -0300

    Recepción del registro de datos del usuario.

--- recepcion_registro_usuario.php
+++ recepcion_registro_usuario.php
@@ -1,7 +1,10 @@
 <?php
 require 'plantillas/cabecera.php';
 $conn = conectar();
-$consulta = $conn->query("INSERT INTO usuarios(nombre,apellido,email,comentario) VALUES('{$_REQUEST['nombre']}','{$_REQUEST['apellido']}','{$_REQUEST['email']}','{$_REQUEST['comentario']}')");
+$consulta = $conn->query("INSERT INTO usuarios(nombre,apellido,dni,fecha_nac,ocupacion,email,nombre_usuario,contrasena,biografia) VALUES('{$_REQUEST['nombre']}','{$_REQUEST['apellido']}','{$_REQUEST['dni']}','{$_REQUEST['fecha_nac']}','{$_REQUEST['ocupacion']}','{$_REQUEST['email']}','{$_REQUEST['nombre_usuario']}','{$_REQUEST['contrasena']}','{$_REQUEST['biografia']}')");
+
+
+
 
 
 
