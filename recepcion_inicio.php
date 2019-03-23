fb72ae804e7fd6140e4e63ac4ad779b05aa08c30
Javier Peralta <javier.peralta666@gmail.com>
2019-03-23 16:11:29 -0300

    Nav 100%. Nav dentro del body. Añadido <?php a etiquetas <?.

--- recepcion_inicio.php
+++ recepcion_inicio.php
@@ -1,9 +1,25 @@
 <?php
-require 'plantillas/cabecera.php';
+require 'conexion.php';
 
 $conn = conectar();
 
-$consulta = $conn->prepare("SELECT nombre FROM usuarios  WHERE nombre_usuario='{$_REQUEST['nombre_usuario']}' AND contrasena= '{$_REQUEST['contrasena']}'");
+$consulta = $conn->prepare("
+          SELECT 
+             id,
+             nombre,
+             apellido,
+             dni,
+             fecha_nac,
+             ocupacion,
+             email,
+             nombre_usuario,
+             foto_perfil,
+             biografia,
+             fecha_creacion 
+          FROM usuarios  
+          WHERE (nombre_usuario='{$_REQUEST['nombre_usuario']}' OR email = '{$_REQUEST['nombre_usuario']}') 
+          AND contrasena= '{$_REQUEST['contrasena']}'
+");
 $consulta->execute();
 $usuario = $consulta->fetch(PDO::FETCH_ASSOC);
 
--- recepcion_registro_usuario.php
+++ recepcion_registro_usuario.php
@@ -1,11 +1,10 @@
 <?php
-require 'plantillas/cabecera.php';
+require 'conexion.php';
+
 $conn = conectar();
+
 $consulta = $conn->query("INSERT INTO usuarios(nombre,apellido,dni,fecha_nac,ocupacion,email,nombre_usuario,contrasena,biografia) VALUES('{$_REQUEST['nombre']}','{$_REQUEST['apellido']}','{$_REQUEST['dni']}','{$_REQUEST['fecha_nac']}','{$_REQUEST['ocupacion']}','{$_REQUEST['email']}','{$_REQUEST['nombre_usuario']}','{$_REQUEST['contrasena']}','{$_REQUEST['biografia']}')");
 
-
-
-
-
+header("Location:". ROOT ."/listado_comentarios.php/?mensaje2= Los datos se modificaron correctamente.");
 
 ?>
