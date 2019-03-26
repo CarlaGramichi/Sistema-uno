0c873ca7a213034017d792bb647d8468d7c0cbc5
Carla Gramichi <caaar.m21@gmail.com>
2019-03-26 15:04:06 -0300

    Ver posts.

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php require 'plantillas/cabecera.php';?>
+<?php require 'plantillas/cabecera.php';
 
+$con = conectar();
+$consulta = $con->query("SELECT titulo,extracto,cuerpo,fecha_creacion FROM posts");
+$posts = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-
-
-
-
-
-
-<?php require 'plantillas/footer.php';?>
+?>
+<?php foreach ($posts as $post): ?>
+    <br><div class="card-deck">
+        <div class="card">
+            <img src="imagenes/imagen4.jpg" class="card-img-top" alt="..">
+            <div class="card-body">
+                <h5 class="card-title"><?= $post['titulo'] ?></h5>
+                <h6 class="card-text"><?= $post['extracto'] ?></h6>
+                <p class="card-text"><?= $post['cuerpo'] ?></p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted"><?= $post['fecha_creacion'] ?></small>
+            </div>
+        </div>
+    </div>
+<?php endforeach ?>
+<?php require 'plantillas/footer.php'; ?>
--- ver_comentario.php
+++ ver_comentario.php
@@ -3,7 +3,8 @@
 require 'plantillas/cabecera.php';
 
 $con = conectar();
-$consulta = $con->query("SELECT id,nombre,apellido,email,comentario,fecha_creacion FROM comentarios  WHERE id = {$_REQUEST['id']}");
+$consulta = $con->query("SELECT id,nombre,apellido,email,comentario,fecha_creacion 
+FROM comentarios  WHERE id = {$_REQUEST['id']}");
 $coment = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
