a6a1e894c11026ce041bb17a30fe5d27ac0ba0d2
Carla Gramichi <caaar.m21@gmail.com>
2019-03-26 12:25:52 -0300

    Iconos en el formulario de registro de usuario.

--- registro_usuario.php
+++ registro_usuario.php
@@ -58,7 +58,7 @@
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-info btn-block">Guardar Datos</button>
+            <button type="submit" class="btn btn-info col-sm-11">Guardar Datos</button>
         </form>
     </div>
 
