61dea0c3c59a6ef64d279ce2c662ffb60698b97c
Carla Gramichi <caaar.m21@gmail.com>
2019-04-01 19:54:14 -0300

    Comentar posts solo usuarios registrados

--- recepcion.php
+++ recepcion.php
@@ -2,13 +2,24 @@
 session_start();
 require 'conexion.php';
 $conn = conectar();
-$consulta = $conn->query("INSERT INTO comentarios(nombre,apellido,email,comentario) 
-VALUES('{$_REQUEST['nombre']}','{$_REQUEST['apellido']}','{$_REQUEST['email']}',
-'{$_REQUEST['comentario']}')");
-if (!$consulta) {
-    var_dump($conn->errorInfo());
+$consulta = $conn->query("INSERT INTO comentarios(comentario,usuario_id,post_id) VALUES('{$_REQUEST['comentario']}',
+'{$_SESSION['usuario']['id']}','{$_REQUEST['post_id']}')");
+
+if ($consulta) {
+    header('Location:' . $_SERVER['HTTP_REFERER']);
 } else {
-    header("Location:" . ROOT . "/?mensaje=Los datos se guardaron correctamente.&error[]=error1&error[]=error2&error[]=error3");
+    die(var_dump($conn->errorInfo()));
 }
+
+//$_REQUEST['usuario_id'] = $_SESSION['usuario']['id'];
+//$_REQUEST['comentario'];
+//
+//
+//if (!$conn->query(insertar('comentarios', $_REQUEST))) {
+//    var_dump($conn->errorInfo());
+//} else {
+//    header("Location:" . ROOT . "/?mensaje=El post se cargó correctamente.");
+//}
+
 
 ?>
--- ver_post.php
+++ ver_post.php
@@ -18,14 +18,15 @@ if ($consulta) {
 //FROM posts as p
 //JOIN usuarios as u ON u.id = p.usuario_id
 ?>
-    <br>
+    <br xmlns="http://www.w3.org/1999/html">
     <div class="row">
         <div class="col-sm-8">
             <div class="card text-center ">
                 <div class="card-header ">
                     <p class="card-text float-left">
                         <strong>Autor: </strong>
-                        <em><a href="<?= ROOT ?>/perfil_usuario_post.php?id=<?= $post['autor_id'] ?>" class="text-info"><?= $post['autor'] ?></a></em>
+                        <em><a href="<?= ROOT ?>/perfil_usuario_post.php?id=<?= $post['autor_id'] ?>"
+                               class="text-info"><?= $post['autor'] ?></a></em>
                     </p>
 
                 </div>
@@ -40,17 +41,39 @@ if ($consulta) {
                         <span><?= formatoFecha($post['fecha_creacion']) ?></span>
                     </p>
                 </div>
-
             </div>
+            <br>
+            <?php if (isset($_SESSION['usuario'])): ?>
+                <h4>No te olvides de dejar tu comentario:</h4>
+                <div class="row justify-content-center">
+                    <form action="<?= ROOT ?>/recepcion.php?post_id=<?= $post['id'] ?>"
+                          method="post" class="col-sm-6 mt-4">
+                        <div class="form-group row">
+                            <span style="font-size: 25px" class="col:sm-2"><i class="far fa-edit"></i></span>
+                            <div class="form-group col-sm-10">
+                <textarea name="comentario" placeholder="Comentario" cols="25" rows="6" class="form-control"
+                    <?= !isset($_SESSION['usuario']) ? 'disabled' : '' ?>></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-info col-sm-11"
+                            <?= !isset($_SESSION['usuario']) ? 'disabled' : '' ?>>Comentá
+                        </button>
+                </div>
+            <?php else: ?>
+                <p>No estás registrado. Si quieres comentar el post
+                    <a href="recepcion_inicio.php" data-toggle="modal" data-target="#exampleModal">
+                        sesión</a></p>
+            <?php endif; ?>
         </div>
         <div class="col-sm-4">
             <?php include 'aside.php' ?>
         </div>
+
+        </form>
     </div>
     <br>
-    <a href="<?= ROOT ?>" class="btn btn-info"><span
-                class="fa fa-arrow-left"></span>&emsp;Volver</a>
-
+    <!--    <a href="--><?//= ROOT ?><!--" class="btn btn-info"><span-->
+    <!--                class="fa fa-arrow-left"></span>&emsp;Volver</a>-->
 
 <?php require 'plantillas/footer.php';
 
